58c0987bcf80bd36769105a37fc24b6ba9c76e63
Vforvendetta1981 <167594866+Vforvendetta1981@users.noreply.github.com>
2024-06-20 12:19:08 -0600

    Update script.js

--- script.js
+++ script.js
@@ -22,4 +22,33 @@ function calculateLimit() {
     let remainingEdibles = ediblesLimit - (flowerEquivalent / 28) * 10000;
 
     if (remainingFlower < 0 || remainingConcentrates < 0 || remainingEdibles < 0) {
-        let overLimit = Math.min(remainingFlower, remainingConcentrates * 
+        let overLimit = Math.min(remainingFlower, remainingConcentrates * 7, remainingEdibles / 28 * 10000);
+        document.getElementById('popup-text').innerText = `You are over the limit by ${Math.abs(overLimit).toFixed(2)} grams of flower equivalent.`;
+        document.getElementById('popup').style.display = 'block';
+        setTimeout(() => {
+            document.getElementById('popup').style.display = 'none';
+        }, 3000);
+    } else {
+        document.getElementById('result').innerHTML = `
+            You can purchase up to:
+            <ul>
+                <li>${remainingFlower.toFixed(2)} grams of flower</li>
+                <li>${remainingConcentrates.toFixed(2)} grams of concentrates</li>
+                <li>${remainingEdibles.toFixed(2)} mg of THC in edibles</li>
+            </ul>
+        `;
+    }
+}
+
+function pressKey(key) {
+    const display = document.getElementById('calc-display');
+    if (key === '=') {
+        display.value = eval(display.value);
+    } else {
+        display.value += key;
+    }
+}
+
+function clearCalc() {
+    document.getElementById('calc-display').value = '';
+}
